50b1ff2e80ae9235db8d5f0ac53afda3cd550401
fabio.ferreira@student.kit.edu <f4bio.ferreira@gmail.com>
2018-10-31 17:51:43 -0700

    image io handling and gif generation for later debugging finished, started segmentation

--- data_prep/segmentation.py
+++ data_prep/segmentation.py
@@ -5,6 +5,25 @@ import json
 import math
 import pathlib
 from tensorflow.python.platform import gfile
-from utils.utils import chunks
+from utils.utils import get_experiment_image_data_from_dir
+import matplotlib.pyplot as plt
+from scipy import ndimage
 
+def get_single_segments_of_image(seg_image):
+    raise NotImplementedError
+
+
+def get_single_segments_of_images(seg_images):
+    raise NotImplementedError
+
+def get_number_of_segment(img):
+    """ no background d """
+    return np.unique(img)
+
+
+if __name__ == '__main__':
+    data = get_experiment_image_data_from_dir(source_path="../data/source", experiment_number=5, type="seg")
+    img = data[0]
+    img = img > img.mean()
+    print()
 
--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,10 @@ import argparse
 import os
 import re
 import collections
+import numpy as np
+import moviepy.editor as mpy
+import matplotlib.pyplot as plt
+
 
 def get_args():
     argparser = argparse.ArgumentParser(description=__doc__)
@@ -21,12 +25,18 @@ def chunks(l, n):
     yield l[i:i + n]
 
 
-def get_file_paths_from_dir(source_path, file_type=".npz"):
+def get_file_paths_from_dir(source_path, file_type=".npz", order=True):
+    assert os.path.exists(source_path)
+
     file_paths = {}
     for root, dirs, files in os.walk(source_path):
 
         trial_idx = os.path.basename(root)
-        if trial_idx != "source":
+
+        files = [i for i in files if file_type in i]
+
+        # make sure array is not empty
+        if files:
             dct = {}
             file_paths[trial_idx] = dct
 
@@ -41,6 +51,7 @@ def get_file_paths_from_dir(source_path, file_type=".npz"):
                     lst = []
                     lst.append(os.path.join(root, file))
                     dct[n] = lst
+
 
     # sort inner and outer dicts to avoid conflicts in tfrecord generation
     for i in file_paths.keys():
@@ -67,3 +78,135 @@ def atof(text):
     except ValueError:
         retval = text
     return retval
+
+def load_all_experiments_from_dir(data_list):
+    """
+    Returns a dict of dicts containing the available attribute information about an experiment, e.g.
+    all_experiments
+        - experiment 0
+            - trajectory sample t=0
+                - attributes
+            - trajectory sample t=1
+                - attributes (e.g. img, segmented image, gripper position, ...)
+                etc.
+
+    Args:
+        data_list: a collection of lists which again contain lists of directory paths (either relative or absolute), e.g.
+            00 = {list}
+                00 = {list}
+                    0 = {str} '../data/source/0/0gripperpos.npz'
+                    1 = {str} '../data/source/0/0objpos.npz'
+                    etc.
+     """
+    all_experiments = {}
+    for i, batch_element in enumerate(data_list):
+        experiment = load_data_from_list_of_paths(batch_element)
+        all_experiments[i] = experiment
+    return all_experiments
+
+
+def load_data_from_list_of_paths(batch_element, get_path=False):
+    """
+    loads all the data from a single experiment which is represented by a list of paths
+    Args:
+        batch_element: a list of lists while every list contains a path as a string (can be relative or absolute)
+        get_path: a boolean flag indicating whether the path given as input should also be contained in the returned sub-dicts
+    Returns:
+        A dictionary containing sub-dictionaries while every sub-dict contains the loaded data as ndarray
+    """
+    experiment = {}
+    for j, trajectory in enumerate(batch_element):
+        trajectory_step_data = {}
+        for traj in trajectory:
+            with np.load(traj) as fhandle:
+                key = list(fhandle.keys())[0]
+                data = fhandle[key]
+                trajectory_step_data[key] = data
+                if get_path:
+                    trajectory_step_data["path"] = traj
+        experiment[j] = trajectory_step_data
+    return experiment
+
+
+
+def get_experiment_image_data_from_dir(source_path, experiment_number, type="seg"):
+    """
+    loads and returns all the image data from a single experiment. Expects the following folder structure:
+        folder 'experiment number'
+            0rgb.npz
+            1rgb.npz
+            etc.
+
+    Args:
+        source_path: the root directory of all the experiments
+        experiment_number: integer indicating the number of the experiment to be used
+        type: keyword used to identify the type of data (e.g. either 0rgb.npz or seg.npz)
+
+    Returns:
+        A list of m ndarrays representing the single images while is m is the number of images in the experiment
+    """
+    assert type in ['seg', 'rgb']
+
+    all_paths = get_file_paths_from_dir(source_path)
+    try:
+        experiment_paths = all_paths[experiment_number]
+    except:
+        print("no data found under the specified number")
+        return
+
+    image_data_paths = []
+    for experiment_step in experiment_paths:
+        if experiment_step is not None:
+            image_data_paths.append([path for path in experiment_step if type in path])
+
+    images_dict = load_data_from_list_of_paths(image_data_paths, get_path=False)
+
+
+    return convert_dict_to_list(images_dict)
+
+
+def save_image_data_to_disk(image_data, destination_path, store_gif=True, img_type="seg"):
+    assert destination_path is not None
+
+    output_dir = create_dir(destination_path, "images_" + img_type)
+    for i, img in enumerate(image_data):
+        img = img.astype(np.uint8)
+        plt.imsave(output_dir + "/" + img_type + str(i).zfill(4), img)
+
+
+    if store_gif:
+        output_dir = create_dir(destination_path, "images" + '_' + img_type)
+        clip = mpy.ImageSequenceClip(output_dir, fps=5, with_mask=False).to_RGB()
+        clip.write_gif(os.path.join(output_dir, 'sequence_' + img_type + '.gif'), program='ffmpeg')
+
+
+def convert_dict_to_list(dct):
+    """ assumes a dict of subdicts of which each subdict only contains one key containing the desired data """
+    lst = []
+    for value in dct.values():
+        element = next(iter(value.values())) # get the first element, assuming the dicts contain only the desired data
+        lst.append(element)
+    return lst
+
+
+def create_dir(output_dir, dir_name):
+  assert(output_dir)
+  output_dir = os.path.join(output_dir, dir_name)
+  if not os.path.isdir(output_dir):
+    os.mkdir(output_dir)
+    print('Created custom directory:', dir_name)
+    return output_dir
+  print('Using existing directory:', dir_name)
+  return output_dir
+
+
+
+
+if __name__ == '__main__':
+    source_path = "../data/source"
+    exp_number = 5
+    dest_path = os.path.join(source_path, str(exp_number))
+
+    image_data = get_experiment_image_data_from_dir(source_path=source_path, experiment_number=exp_number, type="rgb")
+    save_image_data_to_disk(dest_path, img_type="rgb")
+
