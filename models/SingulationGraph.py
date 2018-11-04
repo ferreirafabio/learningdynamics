4a96fb4477d7d9462e0f94210cf3431c4a28bb22
fabio.ferreira@student.kit.edu <f4bio.ferreira@gmail.com>
2018-11-04 01:51:26 -0800

    started creating graphs

--- models/SingulationGraph.py
+++ models/SingulationGraph.py
@@ -8,45 +8,40 @@ from graph_nets import utils_tf, utils_np
 
 
 class SingulationGraph():
-    def __init__(self, config, node_output_size):
+    def __init__(self, config, n_total_objects, n_manipulable_objects):
         self.config = config
         self.n_nodes_attr = self.config["n_nodes_attr"]
         #self.n_edges_attr = self.config["n_edges_attr"]
         #self.n_globals_attr = self.config["n_globals_attr"]
         #self.global_output_size = self.config["global_output_size"]
 
-        self.G = nx.MultiDiGraph()
+        self.G = nx.complete_graph(n_total_objects, nx.MultiDiGraph)
+        # todo: check whether node features can have different shapes
 
-        """ adding nodes and features """
-        for i in range(node_output_size):
-            self.G.add_node(i, features=tf.placeholder(tf.uint8, shape=(120, 160, 4)))
-            ''' can we add multiple features of unequal shape to the same node???'''
-            self.G.add_node(i, features=tf.placeholder(tf.uint8, shape=(120, 160, 4)))
-        self.G.add_nodes_from(np.arange(node_output_size))
+        """ adding features to nodes """
+        # 120*160*4(seg+rgb)
+        self.G.node[0]['type'] = 'container'
+        self.G.node[0]['features'] = np.ndarray(shape=(76800,), dtype=np.float32)
 
-
-        self.nx_graph = [nx.complete_graph(node_output_size, nx.MultiDiGraph())]
-
-        #self.nx_graph = nx.MultiDiGraph()
-        #self.nx_graph.add_nodes_from(np.arange(node_output_size))
+        # 120*160*4(seg+rgb)+3(pos)
+        self.G.node[1]['type'] = 'gripper'
+        self.G.node[1]['features'] = np.ndarray(shape=(76803,), dtype=np.float32)
 
 
-        #self.nodes = np.ndarray(shape=(node_output_size, self.n_nodes_attr), dtype=np.float32)
-        #self.globals = np.ndarray(shape=(self.global_output_size, self.n_globals_attr), dtype=np.float32)
-        #self.edges, self.senders, self.receivers = [], [], []
+        for i in range(2, n_manipulable_objects+2):
+            ''' no multiple features -> flatten and concatenate everything '''
+            # 120*160*4(seg+rgb)+3(pos)+3(vel)
+            self.G.node[i]['type'] = 'manipulable_object'
+            self.G.node[i]['features'] = np.ndarray(shape=(76806,), dtype=np.float32)
 
-        self.graph = self.build_graph()
+        """ adding features to edges """
+        # todo
+
+
+        self.graphs_tuple = utils_np.networkxs_to_graphs_tuple([self.G])
 
     def build_graph(self):
+        return utils_tf.data_dicts_to_graphs_tuple([self.data_dict])
 
-        #data_dict = {
-        #    "globals": self.globals,
-        #    "nodes": self.nodes,
-        #    "edges": self.edges,
-        #    "receivers": self.receivers,
-        #    "senders": self.senders
-        #}
-
-        return utils_np.networkxs_to_graphs_tuple(self.nx_graph)
 
 
