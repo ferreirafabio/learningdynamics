5c4e56e6970cd3c4782da37952d2039c0fb439f3
fabio.ferreira@student.kit.edu <f4bio.ferreira@gmail.com>
2018-11-10 00:42:48 -0800

    code merged with tf project template

--- base/base_train.py
+++ base/base_train.py
@@ -12,7 +12,7 @@ class BaseTrain:
         self.sess.run(self.init)
 
     def train(self):
-        for cur_epoch in range(self.model.cur_epoch_tensor.eval(self.sess), self.config.num_epochs + 1, 1):
+        for cur_epoch in range(self.model.cur_epoch_tensor.eval(self.sess), self.config.n_epochs + 1, 1):
             self.train_epoch()
             self.sess.run(self.model.increment_cur_epoch_tensor)
 
