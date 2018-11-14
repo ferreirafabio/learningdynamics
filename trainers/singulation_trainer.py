ad37f2a6a68ea36768f4309eabbdb8ec491f156f
fabio.ferreira@student.kit.edu <f4bio.ferreira@gmail.com>
2018-11-13 19:07:54 -0800

    test cycle implementation completed

--- trainers/singulation_trainer.py
+++ trainers/singulation_trainer.py
@@ -10,42 +10,19 @@ class SingulationTrainer(BaseTrain):
         super(SingulationTrainer, self).__init__(sess, model, train_data, valid_data, config, logger)
 
     def train_epoch(self):
+        prefix = self.config.exp_name
         while True:
            try:
-                _, cur_it = self.train_batch()
+                _, cur_it = self.train_batch(prefix)
 
-                if cur_it % self.config.model_save_interval == 1:
+                if cur_it % self.config.model_save_step_interval == 0:
                     self.model.save(self.sess)
-                if cur_it % self.config.test_interval == 1:
+                if cur_it % self.config.test_interval == 0:
                     print("Executing test batch")
-                    self.test_batch()
+                    self.test_batch(prefix)
 
            except tf.errors.OutOfRangeError:
                break
-
-    def train_batch(self):
-        losses = []
-        next_element = self.train_data.get_next_batch()
-        features = self.sess.run(next_element)
-
-        features = convert_dict_to_list_subdicts(features, self.config.train_batch_size)
-        _, _, input_graphs_all_exp, target_graphs_all_exp = create_graphs_and_placeholders(config=self.config, batch_data=features,
-                                                                                         batch_size=self.config.train_batch_size)
-        for j in range(self.config.train_batch_size):
-            loss, _ = self.do_step(input_graphs_all_exp[j], target_graphs_all_exp[j], features[j])
-            if loss is not None:
-                losses.append(loss)
-
-        cur_it = self.model.global_step_tensor.eval(self.sess)
-        if losses:
-            batch_loss = np.mean(losses)
-            print('step: ', cur_it, ' loss(batch): ', batch_loss)
-            summaries_dict = {'loss': batch_loss}
-            self.logger.summarize(cur_it, summaries_dict=summaries_dict)
-        else:
-            batch_loss = 0
-
-        return batch_loss, cur_it
 
     def do_step(self, input_graph, target_graphs, feature, train=True):
         exp_length = feature['experiment_length']
@@ -65,10 +42,34 @@ class SingulationTrainer(BaseTrain):
             # "step": self.model.step_op,
             data = self.sess.run({"step": self.model.step_op, "target": self.model.target_ph, "loss": self.model.loss_op_test,
                                   "outputs": self.model.output_ops_test}, feed_dict=feed_dict)
-
+            print(data)
         return data['loss'], data['outputs']
 
-    def test_batch(self):
+    def train_batch(self, prefix):
+        losses = []
+        next_element = self.train_data.get_next_batch()
+        features = self.sess.run(next_element)
+
+        features = convert_dict_to_list_subdicts(features, self.config.train_batch_size)
+        _, _, input_graphs_all_exp, target_graphs_all_exp = create_graphs_and_placeholders(config=self.config, batch_data=features,
+                                                                                         batch_size=self.config.train_batch_size)
+        for i in range(self.config.train_batch_size):
+            loss, _ = self.do_step(input_graphs_all_exp[i], target_graphs_all_exp[i], features[i])
+            if loss is not None:
+                losses.append(loss)
+
+        cur_it = self.model.global_step_tensor.eval(self.sess)
+        if losses:
+            batch_loss = np.mean(losses)
+            print('step {:06d} loss (batch) {:0.2f}'.format(cur_it, batch_loss))
+            summaries_dict = {prefix + '_loss': batch_loss}
+            self.logger.summarize(cur_it, summaries_dict=summaries_dict, summarizer="train")
+        else:
+            batch_loss = 0
+
+        return batch_loss, cur_it
+
+    def test_batch(self, prefix):
         losses = []
         next_element = self.test_data.get_next_batch()
         features = self.sess.run(next_element)
@@ -83,6 +84,7 @@ class SingulationTrainer(BaseTrain):
         cur_it = self.model.global_step_tensor.eval(self.sess)
         for i in range(self.config.test_batch_size):
             loss, outputs = self.do_step(input_graphs_all_exp[i], target_graphs_all_exp[i], features[i], train=False)
+            print(loss, outputs)
             if loss is not None:
                 losses.append(loss)
             ''' get the last not None output '''
@@ -95,35 +97,36 @@ class SingulationTrainer(BaseTrain):
             features_index = output_for_summary[1]
 
             ''' get the predicted images '''
-            summaries_dict_rgb = {'output_img_rgb_{}_object_{}'.format(j, k): rgb for j, rgb_list in enumerate(images_rgb) for k, rgb in enumerate(rgb_list)}
-            summaries_dict_depth = {'output_img_depth_{}_object_{}'.format(j, k): depth for j, depth_list in enumerate(images_depth) for k, depth in enumerate(depth_list)}
-            summaries_dict_seg = {'output_img_seg_{}_object_{}'.format(j, k): seg for j, seg_list in enumerate(images_seg) for k, seg in enumerate(seg_list)}
+            summaries_dict_rgb = {prefix + '_output_img_rgb_{}_object_{}'.format(j, k): rgb for j, rgb_list in enumerate(images_rgb) for k, rgb in enumerate(rgb_list)}
+            summaries_dict_depth = {prefix + '_output_img_depth_{}_object_{}'.format(j, k): depth for j, depth_list in enumerate(images_depth) for k, depth in enumerate(depth_list)}
+            summaries_dict_seg = {prefix + '_output_img_seg_{}_object_{}'.format(j, k): seg for j, seg_list in enumerate(images_seg) for k, seg in enumerate(seg_list)}
 
             ''' get the ground truth images for comparison, [-3:] means 'get the last three manipulable objects '''
-            target_summaries_dict_rgb = {'target_img_rgb_{}_object_{}'.format(j, k): img[:, :, :3] for j, image_data in
+            target_summaries_dict_rgb = {prefix + '_target_img_rgb_{}_object_{}'.format(j, k): img[:, :, :3] for j, image_data in
                                          enumerate(features[features_index]['object_segments']) for k, img in enumerate(image_data[-3:])}
 
-            target_summaries_dict_seg = {'target_img_seg_{}_object_{}'.format(j, k): img[:, :, 4] for j, image_data in
+            target_summaries_dict_seg = {prefix + '_target_img_seg_{}_object_{}'.format(j, k): img[:, :, 4] for j, image_data in
                                          enumerate(features[features_index]['object_segments']) for k, img in enumerate(image_data[-3:])}
 
-            target_summaries_dict_depth = {'target_img_depth_{}_object_{}'.format(j, k): img[:, :, -3:] for j, image_data in
+            target_summaries_dict_depth = {prefix + '_target_img_depth_{}_object_{}'.format(j, k): img[:, :, -3:] for j, image_data in
                                          enumerate(features[features_index]['object_segments']) for k, img in enumerate(image_data[-3:])}
 
+        if losses:
+            batch_loss = np.mean(losses)
+            print('step {:06d} loss (test batch) {:0.2f}'.format(cur_it, batch_loss))
 
+            summaries_dict = {prefix + '_loss': batch_loss}
+            summaries_dict = {
+                **summaries_dict_rgb, **summaries_dict_seg, **summaries_dict_depth,
+                **summaries_dict,
+                **target_summaries_dict_rgb, **target_summaries_dict_seg, **target_summaries_dict_depth,
+            }
+            print(summaries_dict)
+            self.logger.summarize(cur_it, summaries_dict=summaries_dict, summarizer="test")
 
-        batch_loss = np.mean(losses)
-        print('step: ', cur_it, ' loss(batch): ', batch_loss)
-        summaries_dict = {'loss': batch_loss}
-        summaries_dict = {
-            **summaries_dict_rgb, **summaries_dict_seg, **summaries_dict_depth,
-            **summaries_dict,
-            **target_summaries_dict_rgb, **target_summaries_dict_seg, **target_summaries_dict_depth,
-        }
-
-        self.logger.summarize(cur_it, summaries_dict=summaries_dict, summarizer="test")
-
-        if not losses:
+        else:
             batch_loss = 0
+
         return batch_loss, cur_it
 
 
