bdbbe3398cf7ce03c860a4cb6ac78794b7ff3b99
fabio.ferreira@student.kit.edu <f4bio.ferreira@gmail.com>
2018-11-19 22:15:49 -0800

    perf mode

--- trainers/singulation_trainer.py
+++ trainers/singulation_trainer.py
@@ -43,17 +43,17 @@ class SingulationTrainer(BaseTrain):
             data = self.sess.run({"step": self.model.step_op, "target": self.model.target_ph, "loss": self.model.loss_op_train,
                                   "outputs": self.model.output_ops_train, "pos_vel_loss": self.model.pos_vel_loss_ops_train
                                   }, feed_dict=feed_dict, options=options, run_metadata=run_metadata) # todo
-
+            fetched_timeline = timeline.Timeline(run_metadata.step_stats)
+            chrome_trace = fetched_timeline.generate_chrome_trace_format()
+            if i is not None:
+                with open('timeline_01_step_%d.json' % i, 'w') as f:
+                    f.write(chrome_trace)
         else:
             data = self.sess.run({"step": self.model.step_op, "target": self.model.target_ph, "loss": self.model.loss_op_test,
                                   "outputs": self.model.output_ops_test, "pos_vel_loss": self.model.pos_vel_loss_ops_test
                                   }, feed_dict=feed_dict, options=options, run_metadata=run_metadata) # todo
 
-        fetched_timeline = timeline.Timeline(run_metadata.step_stats)
-        chrome_trace = fetched_timeline.generate_chrome_trace_format()
-        if i is not None:
-            with open('timeline_01_step_%d.json' % i, 'w') as f:
-                f.write(chrome_trace)
+
 
         return data['loss'], data['outputs'], data['pos_vel_loss']
 
