4fb6e9a2f6cadff753e1fafca0b96cc948b1911a
fabio.ferreira@student.kit.edu <f4bio.ferreira@gmail.com>
2018-12-19 20:15:32 -0800

    added notebook for visualization purposes

--- mains/singulation_graph_nets.py
+++ mains/singulation_graph_nets.py
@@ -29,7 +29,7 @@ def main():
 
     # create your data generator
     train_data = DataGenerator(config, sess, train=True)
-    valid_data = DataGenerator(config, sess, train=False)
+    test_data = DataGenerator(config, sess, train=False)
 
     model = EncodeProcessDecode(config)
 
@@ -37,7 +37,7 @@ def main():
     logger = Logger(sess, config)
 
     # create trainer and pass all the previous components to it
-    trainer = SingulationTrainer(sess, model, train_data, valid_data, config, logger)
+    trainer = SingulationTrainer(sess, model, train_data, test_data, config, logger)
 
     # load model if exists
     model.load(sess)
