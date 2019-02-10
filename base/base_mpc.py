cd3f629ea400dbb022bf68a7ff30f070f714384e
fabio.ferreira@student.kit.edu <f4bio.ferreira@gmail.com>
2019-02-09 18:05:22 -0800

    parallel MPPI

--- base/base_mpc.py
+++ base/base_mpc.py
@@ -1,6 +1,9 @@
 import tensorflow as tf
-import scipy as sp
 import numpy as np
+import time
+import multiprocessing
+from joblib import parallel_backend, Parallel, delayed
+from multiprocessing import Pool
 
 
 class MPC:
@@ -19,6 +22,134 @@ class MPC:
 
     def solve(self, state):
         raise NotImplementedError
+
+
+class MPPI:
+    def __init__(self, K, T, U, lambda_=1.0, noise_mu=0, noise_sigma=1, u_init=1, noise_gaussian=True):
+        self.K = K  # N_SAMPLES
+        self.T = T  # TIMESTEPS
+        self.lambda_ = lambda_
+        self.noise_mu = noise_mu
+        self.noise_sigma = noise_sigma
+        self.U = U
+        self.u_init = u_init
+        self.cost_total = np.zeros(shape=(self.K))
+
+        if noise_gaussian:
+            self.noise = np.random.normal(loc=self.noise_mu, scale=self.noise_sigma, size=(self.K, self.T))
+        else:
+            self.noise = np.full(shape=(self.K, self.T), fill_value=0.9)
+
+    def _ensure_non_zero(self, cost, beta, factor):
+        return np.exp(-factor * (cost - beta))
+
+
+class MPPI_gym_parallel(MPPI):
+    """ MMPI according to algorithm 2 in Williams et al., 2017
+        'Information Theoretic MPC for Model-Based Reinforcement Learning' """
+
+    def __init__(self, env, K, T, U, lambda_=1.0, noise_mu=1, noise_sigma=100, u_init=1, x_init=None, noise_gaussian=True, downward_start=True):
+        super(MPPI_gym_parallel, self).__init__(K=K, T=T, U=U, lambda_=lambda_, noise_mu=noise_mu, noise_sigma=noise_sigma,
+                                                u_init=u_init, x_init=x_init, noise_gaussian=noise_gaussian)
+        self.env = env
+        self.env.reset()
+        if downward_start:
+            self.env.env.state = [np.pi, 1]
+        self.x_init = env.env.state
+
+
+    def _control(self, _):
+        self._compute_total_cost()
+        beta = np.min(self.cost_total)  # minimum cost of all trajectories
+        cost_total_non_zero = self._ensure_non_zero(cost=self.cost_total, beta=beta, factor=1 / self.lambda_)
+
+        eta = np.sum(cost_total_non_zero)
+        omega = 1 / eta * cost_total_non_zero
+
+        self.U += [np.sum(omega * self.noise[:, t]) for t in range(self.T)]
+
+        self.env.env.state = self.x_init
+        s, r, _, _ = self.env.step([self.U[0]])
+        print("action taken: ", self.U[0], "cost received: ", -r)
+        self.env.render()
+
+        self.U = np.roll(self.U, -1)  # shift all elements to the left
+        self.U[-1] = self.u_init
+        self.x_init = self.env.env.state
+
+
+    def _compute_total_cost(self):
+        for k in range(self.K):
+            self.env.env.state = self.x_init
+            for t in range(self.T):
+                perturbed_action_t = self.U[t] + self.noise[k, t]
+                _, reward, _, _ = self.env.step([perturbed_action_t])
+                self.cost_total[k] += -reward
+
+
+    def control(self, iter=1000, parallel=False):
+        if parallel:
+            with Pool(5) as p:
+                print(p.map(self._control, range(iter)))
+        else:
+            for _ in range(iter):
+                self._control()
+
+
+
+
+class MPPI_gym(MPPI):
+    """ MMPI according to algorithm 2 in Williams et al., 2017
+        'Information Theoretic MPC for Model-Based Reinforcement Learning' """
+
+    def __init__(self, env, K, T, U, lambda_=1.0, noise_mu=1, noise_sigma=100, u_init=1, x_init=None, noise_gaussian=True, downward_start=True):
+        super(MPPI_gym, self).__init__(K=K, T=T, U=U, lambda_=lambda_, noise_mu=noise_mu, noise_sigma=noise_sigma,
+                                       u_init=u_init, noise_gaussian=noise_gaussian)
+        self.env = env
+        self.env.reset()
+        if downward_start:
+            self.env.env.state = [np.pi, 1]
+        self.x_init = self.env.env.state
+
+    def _compute_total_cost(self, k):
+        self.env.env.state = self.x_init
+        for t in range(self.T):
+            perturbed_action_t = self.U[t] + self.noise[k, t]
+            _, reward, _, _ = self.env.step([perturbed_action_t])
+            self.cost_total[k] += -reward
+
+    def control(self, iter=1000, parallel=False):
+        for _ in range(iter):
+            start_time = time.time()
+            last_log_time = start_time
+
+            if parallel:
+                with parallel_backend('threading', n_jobs=multiprocessing.cpu_count()):
+                    Parallel()(delayed(self._compute_total_cost)(k) for k in range(self.K))
+            else:
+                for k in range(self.K):
+                    self._compute_total_cost(k)
+
+            the_time = time.time()
+            elapsed_since_last_log = the_time - last_log_time
+
+            beta = np.min(self.cost_total)  # minimum cost of all trajectories
+            cost_total_non_zero = self._ensure_non_zero(cost=self.cost_total, beta=beta, factor=1/self.lambda_)
+
+            eta = np.sum(cost_total_non_zero)
+            omega = 1/eta * cost_total_non_zero
+
+            self.U += [np.sum(omega * self.noise[:, t]) for t in range(self.T)]
+
+            self.env.env.state = self.x_init
+            s, r, _, _ = self.env.step([self.U[0]])
+            print("action taken: {:.2f} cost received: {:.2f} duration: {:.2f}".format(self.U[0], -r, elapsed_since_last_log))
+            self.env.render()
+
+            self.U = np.roll(self.U, -1)  # shift all elements to the left
+            self.U[-1] = self.u_init
+            self.cost_total[:] = 0
+            self.x_init = self.env.env.state
 
 
 
--- test/pendulum_mppi.py
+++ test/pendulum_mppi.py
@@ -1,62 +1,23 @@
 import gym
 import numpy as np
 
-from base.base_mpc import MPPI
+from base.base_mpc import MPPI_gym
 
 ENV_NAME = "Pendulum-v0"
 
-TIMESTEPS = 50  # T
+TIMESTEPS = 20  # T
 N_SAMPLES = 1000  # K
 ACTION_LOW = -2.0
 ACTION_HIGH = 2.0
 
 noise_mu = 0
-noise_sigma = 1
+noise_sigma = 10
 lambda_ = 1
 
 
-def pendulum_MPPI():
-    """ MMPI according to algorithm 2 in Williams et al., 2017
-    'Information Theoretic MPC for Model-Based Reinforcement Learning' """
+if __name__ == "__main__":
+    U = np.random.uniform(low=ACTION_LOW, high=ACTION_HIGH, size=TIMESTEPS)  # pendulum joint effort in (-2, +2)
 
     env = gym.make(ENV_NAME)
-
-    def _ensure_non_zero(costs, beta, factor):
-        return np.exp(-factor*(costs - beta))
-
-    env.reset()
-    state = env.env.state
-
-    while True:
-        U = np.random.uniform(low=ACTION_LOW, high=ACTION_HIGH, size=TIMESTEPS)  # pendulum joint effort in (-2, +2)
-        cost_total = np.zeros(shape=(N_SAMPLES))
-        noise = np.random.normal(loc=noise_mu, scale=noise_sigma, size=(N_SAMPLES, TIMESTEPS))
-
-        for k in range(N_SAMPLES):
-            env.env.state = state
-            for t in range(1,TIMESTEPS):
-                perturbed_action_t = U[t-1] + noise[k, t-1]
-                state_next, reward, _, _ = env.step([perturbed_action_t])
-                cost_total[k] += -reward
-
-        beta = np.min(cost_total)  # minimum cost of all trajectories
-        cost_total_non_zero = _ensure_non_zero(costs=cost_total, beta=beta, factor=1/lambda_)
-
-        eta = np.sum(cost_total_non_zero)
-
-        omega = 1/eta * cost_total_non_zero
-
-        U += [np.sum(omega * noise[:, t]) for t in range(TIMESTEPS)]
-
-        env.env.state = state
-        s, r, _, _ = env.step([U[0]])
-        print("action taken: ", U[0], "cost received: ", -r)
-        env.render()
-
-        U = np.roll(U, -1)  # shift all elements to the left
-        U[-1] = np.random.uniform(low=ACTION_LOW, high=ACTION_HIGH)
-        state = env.env.state
-
-
-if __name__ == "__main__":
-    pendulum_MPPI()
+    mppi_gym = MPPI_gym(env=env, K=N_SAMPLES, T=TIMESTEPS, U=U, lambda_=lambda_, noise_mu=noise_mu, noise_sigma=noise_sigma, u_init=0, noise_gaussian=True)
+    mppi_gym.control(1000, parallel=True)
