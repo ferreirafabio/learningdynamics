fixed normalization for images, position and velocity

data_loader/data_generator.py
@@ -132,37 +132,37 @@ class DataGenerator:
         objpos = tf.reshape(objpos, tf.stack([experiment_length, n_manipulable_objects, 3]))
 
         # the following yields [240,240,240] to broadcast with shape (experiment_length, n_manipulable_objects, 3)
+        # might be unneccessary step in case of normalization due to multiplicative scale invariance
         vel_broadcast_tensor = tf.fill((3,), tf.cast(240.0, tf.float64))
         objvel = tf.identity(objpos, name="objvel") * vel_broadcast_tensor  # frequency used: 1/240 --> velocity: pos/time --> pos/(1/f) --> pos*f
 
-        # indices: 0=first object,...,2=third object
-        #objvel = tf.decode_raw(sequence['objvel'], out_type=tf.float64)
-        #objvel = tf.reshape(objvel, tf.stack([experiment_length, n_manipulable_objects, 3]))
-
         if self.old_tfrecords:
             img_type = tf.uint8
-            image_range = [np.iinfo(np.uint8).min, np.iinfo(np.uint8).max]
+            image_range_seg = [np.iinfo(np.uint8).min, np.iinfo(np.uint8).max]
             object_segments = tf.decode_raw(sequence['object_segments'], out_type=img_type)
         else:
             img_type = tf.int16
-            image_range = [np.iinfo(np.int16).min, np.iinfo(np.int16).max]
+            image_range_seg = [np.iinfo(np.int16).min, np.iinfo(np.int16).max]
             object_segments = tf.decode_raw(sequence['object_segments'], out_type=img_type)
         object_segments = tf.reshape(object_segments, shape_if_depth_provided)
 
         if normalize:
+            image_range = [np.iinfo(np.uint16).min, np.iinfo(np.uint16).max]
             img_shape = img.get_shape()[-3:]
             seg_shape = seg.get_shape()[-2:]
             object_seg_shape = object_segments.get_shape()[-3:]
             gripperpos_shape = gripperpos.get_shape()[-1:]
             objpos_shape = objpos.get_shape()[-1:]
+
             img = _normalize_fixed(img, range_min=image_range[0], range_max=image_range[1], normed_min=0, normed_max=1, shape=img_shape)
             seg = _normalize_fixed(seg, range_min=image_range[0], range_max=image_range[1], normed_min=0, normed_max=1, shape=seg_shape)
 
             if self.depth_data_provided:
                 depth_shape = depth.get_shape()[-3:]
                 depth = _normalize_fixed(depth, range_min=image_range[0], range_max=image_range[1], normed_min=0, normed_max=1, shape=depth_shape)
-            object_segments = _normalize_fixed(object_segments, range_min=image_range[0], range_max=image_range[1], normed_min=0,
-                                               normed_max=1, shape=object_seg_shape)
+
+            #object_segments = _normalize_fixed(object_segments, range_min=image_range_seg[0], range_max=image_range_seg[1], normed_min=0,
+            #                                   normed_max=1, shape=object_seg_shape)
 
             gripperpos = _normalize_fixed_pos_vel_data(gripperpos, normed_min=0, normed_max=1, shape=gripperpos_shape)
             objpos = _normalize_fixed_pos_vel_data(objpos, normed_min=0, normed_max=1, shape=objpos_shape)
@@ -199,7 +199,7 @@ def _normalize_fixed(x, range_min, range_max, normed_min, normed_max, shape):
     current_min_tensor = tf.fill(shape, tf.cast(range_min, x.dtype))
     current_max_tensor = tf.fill(shape, tf.cast(range_max, x.dtype))
 
-    # using broadcasting, e.g. shape (5,10,120,160,3) subtracted by shape (120,160,3) yields (5,10,120,160,3)
+    # using broadcasting, e.g. shape (10,5,120,160,7) subtracted by shape (120,160,7) yields (10,5,120,160,7)
     x_normed = (x - current_min_tensor) / (current_max_tensor - current_min_tensor)
 
     normed_min_tensor = tf.fill(shape, tf.cast(normed_min, x_normed.dtype))

models/singulation_graph.py
@@ -107,7 +107,7 @@ def graph_to_input_and_targets_single_experiment(config, graph, features, initia
                 """ in this case, the nodes will have dynamic visual information over time """
                 obj_seg = features['object_segments'][step][obj_id_segs].astype(np.float32).flatten()
             pos = features['objpos'][step][obj_id].flatten().astype(np.float32)
-            vel = features['objvel'][step][obj_id].flatten().astype(np.float32) # todo: normalize by fps 1/30
+            vel = features['objvel'][step][obj_id].flatten().astype(np.float32)
             return np.concatenate((obj_seg, vel, pos))
 
 
@@ -217,6 +217,7 @@ def create_placeholders(config, batch_data):
 
     input_ph = utils_tf.placeholders_from_networkxs(input_graphs, force_dynamic_num_graphs=True)
     target_ph = utils_tf.placeholders_from_networkxs(target_graphs[0], force_dynamic_num_graphs=True)
+
     return input_ph, target_ph
 
 

utils/tf_summaries.py
@@ -71,16 +71,15 @@ def create_image_summary(output_for_summary, config, prefix, features, cur_batch
     target_summaries_dict_global_seg, target_summaries_dict_global_depth = create_target_summary_dicts(
         prefix=prefix, features=features, features_index=features_index, cur_batch_it=cur_batch_it)
 
-    summaries_dict_images = {**predicted_summaries_dict_rgb, **predicted_summaries_dict_seg, **predicted_summaries_dict_depth,
-                             **target_summaries_dict_rgb, **target_summaries_dict_seg, **target_summaries_dict_depth,
-                             **target_summaries_dict_global_img, **target_summaries_dict_global_seg,
-                             **target_summaries_dict_global_depth}
+    summaries_dict_images = {**predicted_summaries_dict_rgb, **predicted_summaries_dict_seg,
+                             **target_summaries_dict_rgb, **target_summaries_dict_seg,
+                             **target_summaries_dict_global_img, **target_summaries_dict_global_seg}
 
 
     return summaries_dict_images, features_index
 
 
-def create_latent_data_df(output_for_summary, gt_features, adjust_pos_ped_range=False, adjust_vel_pred_range=False, norm_factor=240):
+def create_latent_data_df(output_for_summary, gt_features, adjust_pos_ped_range=False, adjust_vel_pred_range=False, convert_pos_to_vel=True, norm_factor=240):
     """ creates a dataframe with rows = timesteps (rollouts) and as columns the predictions / ground truths
      of velocities and columns, e.g.
         0_obj_pred_pos, 0_obj_gt_pos, 1_obj_pred_pos, 1_obj_gt_pos, ... , 0_obj_pred_vel, 0_obj_gt_vel, ...
@@ -92,13 +91,17 @@ def create_latent_data_df(output_for_summary, gt_features, adjust_pos_ped_range=
     pos, vel = get_latent_from_gn_output(output_for_summary[0])  # exclude the index
     # unresolved bug: velocity ground truth is scaled by a factor of "norm_factor" (as expected) but the predicted vel is not scaled
     # vice-versa for position
-    if adjust_pos_ped_range:
-        pos = [list(ary/norm_factor for ary in lst) for lst in pos]
-    if adjust_vel_pred_range:
-        vel = [list(ary * norm_factor for ary in lst) for lst in vel]
+    #if adjust_pos_ped_range:
+    #    pos = [list(ary/norm_factor for ary in lst) for lst in pos]
+    #if adjust_vel_pred_range:
+    #    vel = [list(ary * norm_factor for ary in lst) for lst in vel]
 
     features_index = output_for_summary[1]
     pos_gt, vel_gt = get_latent_target_data(gt_features, features_index)
+
+    if convert_pos_to_vel:
+        vel = [list(ary * norm_factor for ary in lst) for lst in vel]
+        vel_gt = [list(ary * norm_factor for ary in lst) for lst in vel_gt]
 
     n_objects = np.shape(output_for_summary[0][0][0])[0]
 
@@ -149,6 +152,8 @@ def generate_results(output, config, prefix, features, cur_batch_it, export_imag
 
     dir_path = check_exp_folder_exists_and_create(features, features_index, prefix, dir_name, cur_batch_it)
 
+    summary_pos_dict_images = None
+
     if export_images and dir_path:  # skip if directory exists
         export_summary_images(config=config, summaries_dict_images=summaries_dict_images, dir_path=dir_path)
 
@@ -157,9 +162,10 @@ def generate_results(output, config, prefix, features, cur_batch_it, export_imag
         export_latent_df(df=df, dir_path=dir_path)
 
         if export_images:
-            export_latent_images(df=df, features=features, features_index=features_index, dir_path=dir_path, config=config)
+            summary_pos_dict_images = export_latent_images(df=df, features=features, features_index=features_index, dir_path=dir_path,
+                                                          config=config, prefix=prefix, cur_batch_it=cur_batch_it)
 
-    return summaries_dict_images
+    return summaries_dict_images, summary_pos_dict_images
 
 
 def get_latent_target_data(features, features_index):