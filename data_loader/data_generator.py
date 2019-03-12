fixed normalization for images, position and velocity

data_loader/data_generator.py
@@ -147,6 +147,7 @@ class DataGenerator:
         else:
             # img, seg
             image_range = [np.uint16(0), np.uint16(255)]
+            image_range_depth = [np.uint16(np.iinfo(np.uint16).min), np.uint16(np.iinfo(np.uint16).max)]
 
             # object_segments
             img_type = tf.int16
@@ -167,7 +168,7 @@ class DataGenerator:
 
             if self.depth_data_provided:
                 depth_shape = depth.get_shape()[-3:]
-                depth = _normalize_fixed(depth, range_min=image_range[0], range_max=image_range[1], normed_min=0, normed_max=1, shape=depth_shape)
+                depth = _normalize_fixed(depth, range_min=image_range_depth[0], range_max=image_range_depth[1], normed_min=0, normed_max=1, shape=depth_shape)
 
             object_segments = _normalize_fixed(object_segments, range_min=image_range_seg[0], range_max=image_range_seg[1], normed_min=0,
                                                normed_max=1, shape=object_seg_shape)

trainers/singulation_trainer.py
@@ -119,7 +119,7 @@ class SingulationTrainer(BaseTrain):
             pos_batch_loss = np.mean(losses_position)
             dis_batch_loss = np.mean(losses_distance)
 
-            print('batch: {:<8} total loss: {:<8.2f} | img loss: {:<8.2f} | vel loss: {:<8.2f} | pos loss: {:<8.4f} | distance loss: {:<8.4f} time(s): {:<10.2f} '
+            print('batch: {:<8} total loss: {:<8.6f} | img loss: {:<8.6f} | vel loss: {:<8.6f} | pos loss: {:<8.6f} | distance loss: {:<8.6f} time(s): {:<10.2f} '
                 .format(
                 cur_batch_it, batch_loss, img_batch_loss, vel_batch_loss, pos_batch_loss, dis_batch_loss, elapsed_since_last_log)
             )
@@ -155,6 +155,7 @@ class SingulationTrainer(BaseTrain):
                                                                     initial_pos_vel_known=initial_pos_vel_known
                                                                     )
 
+
         start_time = time.time()
         last_log_time = start_time
 
@@ -163,8 +164,6 @@ class SingulationTrainer(BaseTrain):
                                                                                                       target_graphs_all_exp[i],
                                                                                                       features[i],
                                                                                                       train=False)
-
-            # todo: denormalize outputs
             if total_loss is not None:
                 losses_total.append(total_loss)
                 losses_img.append(loss_img)
@@ -205,13 +204,11 @@ class SingulationTrainer(BaseTrain):
         else:
             batch_loss, img_batch_loss, vel_batch_loss, pos_batch_loss, dis_batch_loss = 0, 0, 0, 0, 0
 
-
         if outputs_for_summary is not None:
             if self.config.parallel_batch_processing:
                 with parallel_backend('loky', n_jobs=-1):
                     summaries_dict_images = Parallel()(delayed(generate_results)(output, self.config, prefix, features, cur_batch_it,
-                                                                                 export_images, export_latent_data, sub_dir_name)
-                                                       for output in outputs_for_summary)
+                                                                                 export_images, sub_dir_name) for output in outputs_for_summary)
 
 
             else:
@@ -225,12 +222,14 @@ class SingulationTrainer(BaseTrain):
                                                                      export_latent_data=export_latent_data,
                                                                      dir_name=sub_dir_name)
 
-
             if summaries_dict_images:
                 if self.config.parallel_batch_processing:
                     """ parallel mode returns list, just use first element as a summary for the logger """
                     summaries_dict_images = summaries_dict_images[0]
-                summaries_dict = {**summaries_dict, **summaries_dict_images, **summary_pos_dict_images}
+                    if summary_pos_dict_images is not None:
+                        summaries_dict = {**summaries_dict, **summaries_dict_images, **summary_pos_dict_images}
+                    else:
+                        summaries_dict = {**summaries_dict, **summaries_dict_images}
                 cur_batch_it = self.model.cur_batch_tensor.eval(self.sess)
                 self.logger.summarize(cur_batch_it, summaries_dict=summaries_dict, summarizer="test")
                 del summaries_dict

utils/tf_summaries.py
@@ -4,10 +4,10 @@ import numpy as np
 import pandas as pd
 
 from utils.utils import get_images_from_gn_output, get_latent_from_gn_output, check_exp_folder_exists_and_create
-from utils.io import export_summary_images, export_latent_df, export_latent_images
+from utils.io import export_summary_images, export_latent_df, create_latent_images
 from skimage import img_as_ubyte, img_as_uint
 
-def create_predicted_summary_dicts(images_seg, images_depth, images_rgb, prefix, features, features_index, cur_batch_it):
+def create_predicted_summary_dicts(images_seg, images_depth, images_rgb, prefix, features, features_index, cur_batch_it, config):
     predicted_summaries_dict_seg = {
     prefix + '_predicted_seg_exp_id_{}_batch_{}_object_{}'.format(int(features[features_index]['experiment_id']), cur_batch_it, i): obj for
     i, obj in enumerate(images_seg)}
@@ -32,27 +32,31 @@ def create_target_summary_dicts(prefix, features, features_index, cur_batch_it, 
 
     target_summaries_dict_rgb = {
     prefix + '_target_rgb_exp_id_{}_batch_{}_object_{}'.format(features[features_index]['experiment_id'], cur_batch_it, i): np.squeeze(
-        lst[..., :3], axis=0) for i, lst in enumerate(lists_obj_segs)}
+        lst[..., :3], axis=0) if not config.normalize_data else img_as_uint(np.squeeze(lst[..., :3], axis=0))
+        for i, lst in enumerate(lists_obj_segs)}
 
     target_summaries_dict_seg = {
     prefix + '_target_seg_exp_id_{}_batch_{}_object_{}'.format(features[features_index]['experiment_id'], cur_batch_it, i): np.squeeze(
-        np.expand_dims(lst[..., 3], axis=4), axis=0) for i, lst in enumerate(lists_obj_segs)}
+        np.expand_dims(lst[..., 3], axis=4), axis=0) if not config.normalize_data else
+        img_as_uint(np.squeeze(np.expand_dims(lst[..., 3], axis=4), axis=0)) for i, lst in enumerate(lists_obj_segs)}
 
     target_summaries_dict_depth = {
     prefix + '_target_depth_exp_id_{}_batch_{}_object_{}'.format(features[features_index]['experiment_id'], cur_batch_it, i): np.squeeze(
-        lst[..., -3:], axis=0) for i, lst in enumerate(lists_obj_segs)}
+        lst[..., -3:], axis=0) if not config.normalize_data else img_as_uint(np.squeeze(lst[..., -3:], axis=0))
+            for i, lst in enumerate(lists_obj_segs)}
 
     target_summaries_dict_global_img = {
         prefix + '_target_global_img_exp_id_{}_batch_{}'.format(features[features_index]['experiment_id'], cur_batch_it):
-            features[features_index]['img']}
+            features[features_index]['img'] if not config.normalize_data else img_as_uint(features[features_index]['img'])}
 
     target_summaries_dict_global_seg = {
         prefix + '_target_global_seg_exp_id_{}_batch_{}'.format(features[features_index]['experiment_id'], cur_batch_it): np.expand_dims(
-            features[features_index]['seg'], axis=4)}
+            features[features_index]['seg'], axis=4) if not config.normalize_data else img_as_uint(np.expand_dims(
+            features[features_index]['seg'], axis=4))}
 
     target_summaries_dict_global_depth = {
         prefix + '_target_global_depth_exp_id_{}_batch_{}'.format(cur_batch_it, features[features_index]['experiment_id'], cur_batch_it):
-            features[features_index]['depth']}
+            features[features_index]['depth'] if not config.normalize_data else img_as_uint(features[features_index]['depth'])}
 
     return target_summaries_dict_rgb, target_summaries_dict_seg, target_summaries_dict_depth, target_summaries_dict_global_img, \
            target_summaries_dict_global_seg, target_summaries_dict_global_depth
@@ -72,8 +76,8 @@ def create_image_summary(output_for_summary, config, prefix, features, cur_batch
     target_summaries_dict_global_seg, target_summaries_dict_global_depth = create_target_summary_dicts(
         prefix=prefix, features=features, features_index=features_index, cur_batch_it=cur_batch_it, config=config)
 
-    summaries_dict_images = {**predicted_summaries_dict_rgb, **predicted_summaries_dict_seg,
-                             **target_summaries_dict_rgb, **target_summaries_dict_seg,
+    summaries_dict_images = {**predicted_summaries_dict_rgb, **predicted_summaries_dict_seg, **predicted_summaries_dict_depth,
+                             **target_summaries_dict_rgb, **target_summaries_dict_seg, **target_summaries_dict_depth,
                              **target_summaries_dict_global_img, **target_summaries_dict_global_seg}
 
 
@@ -163,7 +167,7 @@ def generate_results(output, config, prefix, features, cur_batch_it, export_imag
         export_latent_df(df=df, dir_path=dir_path)
 
         if export_images:
-            summary_pos_dict_images = export_latent_images(df=df, features=features, features_index=features_index, dir_path=dir_path,
+            summary_pos_dict_images = create_latent_images(df=df, features=features, features_index=features_index, dir_path=dir_path,
                                                           config=config, prefix=prefix, cur_batch_it=cur_batch_it)
 
     return summaries_dict_images, summary_pos_dict_images