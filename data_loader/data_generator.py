fixed normalization for images, position and velocity

data_loader/data_generator.py
@@ -142,7 +142,8 @@ class DataGenerator:
 
             # object_segments
             img_type = tf.uint8
-            image_range_seg = [np.uint8(np.iinfo(np.uint8).min), np.uint8(np.iinfo(np.uint8).max)]
+            #image_range_seg = [np.uint8(np.iinfo(np.uint8).min), np.uint8(np.iinfo(np.uint8).max)]
+            image_range_seg = [np.uint8(0), np.uint8(239)]
             object_segments = tf.decode_raw(sequence['object_segments'], out_type=img_type)
         else:
             # img, seg
@@ -151,7 +152,8 @@ class DataGenerator:
 
             # object_segments
             img_type = tf.int16
-            image_range_seg = [np.int16(-255), np.int16(255)]
+            #image_range_seg = [np.int16(-255), np.int16(255)]
+            image_range_seg = [np.int16(-54), np.int16(243)]
             object_segments = tf.decode_raw(sequence['object_segments'], out_type=img_type)
         object_segments = tf.reshape(object_segments, shape_if_depth_provided)
 

models/singulation_graph.py
@@ -29,7 +29,7 @@ def generate_singulation_graph(config, n_manipulable_objects):
 
     for i in range(offset, n_manipulable_objects):
         ''' no multiple features -> flatten and concatenate everything '''
-        # 120*160*4(seg+rgb)+3(pos)+3(vel)
+        # 120*160*4(seg+rgb)+3(vel)+3(pos)
         object_str = str(i)
         graph_nx.add_node(i, type_name='manipulable_object_' + object_str)
         graph_nx.add_node(i, features=np.zeros(shape=(config.node_output_size,), dtype=np.float32))