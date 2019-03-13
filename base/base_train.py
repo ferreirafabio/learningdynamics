fixed normalization for images, position and velocity

base/base_train.py
@@ -67,7 +67,7 @@ class BaseTrain:
         self.model.output_ops_train = self.model(self.model.input_ph, self.config.n_rollouts, self.model.is_training, self.sess)
         total_loss_ops, loss_ops_img, loss_ops_velocity, loss_ops_position, loss_ops_distance = create_loss_ops(self.config, self.model.target_ph, self.model.output_ops_train)
         self.model.loss_op_train_total = tf.reduce_mean(total_loss_ops)
-        self.model.loss_ops_train_img = tf.reduce_mean(loss_ops_img) # just for summary, is already included in loss_op_train
+        self.model.loss_ops_train_img = tf.reduce_mean(loss_ops_img)  # just for summary, is already included in loss_op_train
         self.model.loss_ops_train_velocity = tf.reduce_mean(loss_ops_velocity)
         self.model.loss_ops_train_position = tf.reduce_mean(loss_ops_position)
         self.model.loss_ops_train_distance = tf.reduce_mean(loss_ops_distance)
@@ -75,13 +75,20 @@ class BaseTrain:
         self.model.step_op = self.model.optimizer.minimize(self.model.loss_op_train_total, global_step=self.model.global_step_tensor)
 
     def initialize_test_model(self):
-        assert self.model.input_ph is not None, "initialize model for training first"
-        assert self.model.target_ph is not None, "initialize model for training first"
+        next_element = self.test_data.get_next_batch()
+        features = self.sess.run(next_element)
+        features = convert_dict_to_list_subdicts(features, self.config.test_batch_size)
+
+        input_ph, target_ph = create_placeholders(self.config, features)
+        input_ph, target_ph = make_all_runnable_in_session(input_ph, target_ph)
+
+        self.model.input_ph_test = input_ph
+        self.model.target_ph_test = target_ph
 
         self.model.is_training = False
-        self.model.output_ops_test = self.model(self.model.input_ph, self.config.n_rollouts, self.model.is_training, self.sess)
-        total_loss_ops_test, loss_ops_test_img, loss_ops_test_velocity, loss_ops_test_position, loss_ops_test_distance = create_loss_ops(self.config, self.model.target_ph, self.model.output_ops_test)
-        self.model.loss_op_test_total = tf.reduce_mean(total_loss_ops_test) # just for summary, is already included in loss_op_train
+        self.model.output_ops_test = self.model(self.model.input_ph_test, self.config.n_rollouts, self.model.is_training, self.sess)
+        total_loss_ops_test, loss_ops_test_img, loss_ops_test_velocity, loss_ops_test_position, loss_ops_test_distance = create_loss_ops(self.config, self.model.target_ph_test, self.model.output_ops_test)
+        self.model.loss_op_test_total = tf.reduce_mean(total_loss_ops_test)  # just for summary, is already included in loss_op_train
         self.model.loss_ops_test_img = tf.reduce_mean(loss_ops_test_img)
         self.model.loss_ops_test_velocity = tf.reduce_mean(loss_ops_test_velocity)
         self.model.loss_ops_test_position = tf.reduce_mean(loss_ops_test_position)

models/loss_functions.py
@@ -6,7 +6,7 @@ def create_loss_ops(config, target_op, output_ops):
     """ ground truth nodes are given by tensor target_op of shape (n_nodes*experience_length, node_output_size) but output_ops
     is a list of graph tuples with shape (n_nodes, exp_len) --> split at the first dimension in order to compute node-wise MSE error
     --> same applies for edges
-    we further assume object/node features are given in the following order (pixels/visual information, velocity(3dim), gripper (3dim))
+    we further assume object/node features are given in the following order (pixels/visual information, velocity(3dim), pos (3dim))
 
     """
     mult = tf.constant([len(output_ops)])