fixed normalization for images, position and velocity

utils/io.py
@@ -5,7 +5,7 @@ import re
 import numpy as np
 from matplotlib import pyplot as plt, animation as animation
 from moviepy import editor as mpy
-from skimage import img_as_ubyte
+from skimage import img_as_ubyte, img_as_int
 
 from eval.AnimateLatentData import AnimateLatentData
 from utils.conversions import convert_dict_to_list
@@ -183,17 +183,21 @@ def save_image_data_to_disk(image_data, destination_path, store_gif=True, img_ty
         clip.write_gif(os.path.join(output_dir, 'sequence_' + img_type + '.gif'), program='ffmpeg')
 
 
-def save_to_gif_from_dict(image_dicts, destination_path, fps=10, use_moviepy=False):
+def save_to_gif_from_dict(image_dicts, destination_path, fps=10, use_moviepy=False, normalization=False):
     if not isinstance(image_dicts, dict) or image_dicts is None:
         return None
 
     for file_name, img_data in image_dicts.items():
-        # if img_data.dtype == np.float32 or img_data.dtype == np.float64:
-        #     ''' normalize [-1, 1]'''
-        #     img_data = 2*(img_data - np.min(img_data))/np.ptp(img_data)-1
-        #
-        # img_data_uint = img_as_ubyte(img_data)
-        img_data_uint = img_data
+        if not normalization:
+            if img_data.dtype == np.float32 or img_data.dtype == np.float64:
+                ''' normalize [-1, 1]'''
+                img_data = 2*(img_data - np.min(img_data))/np.ptp(img_data)-1
+
+            img_data_uint = img_as_int(img_data)
+        else:
+            # if normalized, all data is already float and in [0, 1]
+            img_data_uint = img_data
+
         if len(img_data_uint.shape) == 4 and img_data_uint.shape[3] == 1:
             if use_moviepy:
                 ''' segmentation masks '''
@@ -246,7 +250,7 @@ def create_dir(output_dir, dir_name, verbose=False):
 
 
 def export_summary_images(config, summaries_dict_images, dir_path):
-    save_to_gif_from_dict(image_dicts=summaries_dict_images, destination_path=dir_path, fps=config.n_rollouts)
+    save_to_gif_from_dict(image_dicts=summaries_dict_images, destination_path=dir_path, fps=config.n_rollouts, normalization=config.normalize_data)
 
 
 def export_latent_df(df, dir_path):