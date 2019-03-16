dc9143c3d8f66a2253320addd8ccfd28d1796ca1
Fábio Ferreira <f4bio.ferreira@gmail.com>
2019-03-15 18:34:21 -0700

    Update test_redundancy.py

--- mains/test_redundancy.py
+++ mains/test_redundancy.py
@@ -25,7 +25,7 @@ def main():
         print(e)
         exit(0)
 
-        # create the experiments dirs
+    # create the experiments dirs
     create_dirs([config.summary_dir, config.checkpoint_dir, config.config_file_dir])
 
     # create tensorflow session
@@ -35,15 +35,28 @@ def main():
     train_data = DataGenerator(config, sess, train=True)
     test_data = DataGenerator(config, sess, train=False)
 
-    while True:
-        next_element = test_data.get_next_batch()
-        features_test = sess.run(next_element)
+    with open('redundancy_log.txt', "a+") as text_file:
         while True:
-            next_element = train_data.get_next_batch()
-            features_train = sess.run(next_element)
-            if np.allclose(features_test['objpos'][0][0], features_train['objpos'][0][0]):
-                print("equal")
+            try:
+                next_element = test_data.get_next_batch()
+                features_test = sess.run(next_element)
+                while True:
+                    try:
+                        next_element = train_data.get_next_batch()
+                        features_train = sess.run(next_element)
+                        print("comparing exp id " + str(features_test["experiment_id"]) + " vs " + str(features_train["experiment_id"]))
+                        if np.allclose(features_test['objpos'][0][0], features_train['objpos'][0][0]):
+                            print("experiment ids " + str(features_train["experiment_id"]) +"(train) and " +
+                                            str(features_test["experiment_id"]) + "(test) have identical objpos\n")
 
+                            text_file.write("experiment ids " + str(features_train["experiment_id"]) +"(train) and " +
+                                            str(features_test["experiment_id"]) + "(test) have identical objpos\n")
+                    except tf.errors.OutOfRangeError:
+                        sess.run(train_data.iterator.initializer)
+                        break
+
+            except tf.errors.OutOfRangeError:
+                break
 
 if __name__ == '__main__':
     main()
