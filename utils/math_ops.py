d344ba16dd4d97892d0a8a34c73eaf2e5ee21a54
fabio.ferreira@student.kit.edu <f4bio.ferreira@gmail.com>
2019-04-02 15:09:03 -0700

    df normalization

--- utils/math_ops.py
+++ utils/math_ops.py
@@ -2,19 +2,18 @@ import math
 
 import numpy as np
 
+X_MIN = 0.344
+Y_MIN = -0.256
+Z_MIN = -0.149
+X_MAX = 0.856
+Y_MAX = 0.256
+Z_MAX = -0.0307
 
 def normalize_df_column(df, column_name):
     """ normalizes an entire pandas column (series) in which each row r contains a 3-dim ndarray s.t. all values of r in range (0,1) """
-    x_min = 0.344
-    y_min = -0.256
-    z_min = -0.149
-    x_max = 0.856
-    y_max = 0.256
-    z_max = -0.0307
-
-    x_norm = lambda x: (x - x_min) / (x_max - x_min)
-    y_norm = lambda y: (y - y_min) / (y_max - y_min)
-    z_norm = lambda z: (z - z_min) / (z_max - z_min)
+    x_norm = lambda x: (x - X_MIN) / (X_MAX - X_MIN)
+    y_norm = lambda y: (y - Y_MIN) / (Y_MAX - Y_MIN)
+    z_norm = lambda z: (z - Z_MIN) / (Z_MAX - Z_MIN)
 
     def _normalize_row(row):
         return np.asarray([x_norm(row[0]), y_norm(row[1]), z_norm(row[2])])
@@ -41,32 +40,18 @@ def check_power(N, k):
 
 def normalize_list(lst):
     """ normalizes a list of 3-dim ndarrays x s.t. all x's contain values in (0,1) """
-    x_min = 0.344
-    y_min = -0.256
-    z_min = -0.149
-    x_max = 0.856
-    y_max = 0.256
-    z_max = -0.0307
-
-    x_norm = lambda x: (x - x_min) / (x_max - x_min)
-    y_norm = lambda y: (y - y_min) / (y_max - y_min)
-    z_norm = lambda z: (z - z_min) / (z_max - z_min)
+    x_norm = lambda x: (x - X_MIN) / (X_MAX - X_MIN)
+    y_norm = lambda y: (y - Y_MIN) / (Y_MAX - Y_MIN)
+    z_norm = lambda z: (z - Y_MIN) / (Z_MAX - Z_MIN)
 
     return [np.asarray([x_norm(coords[0]), y_norm(coords[1]), z_norm(coords[2])]) for coords in lst]
 
 
 def normalize_df(df):
     """ normalizes an entire pandas dataframe in which each cell c contains a 3-dim ndarray s.t. c only contains values in (0,1) """
-    x_min = 0.344
-    y_min = -0.256
-    z_min = -0.149
-    x_max = 0.856
-    y_max = 0.256
-    z_max = -0.0307
-
-    x_norm = lambda x: (x - x_min) / (x_max - x_min)
-    y_norm = lambda y: (y - y_min) / (y_max - y_min)
-    z_norm = lambda z: (z - z_min) / (z_max - z_min)
+    x_norm = lambda x: (x - X_MIN) / (X_MAX - X_MIN)
+    y_norm = lambda y: (y - Y_MIN) / (Y_MAX - Y_MIN)
+    z_norm = lambda z: (z - Y_MIN) / (Z_MAX - Z_MIN)
 
     def _normalize_column(column):
         for index, row_value in column.items():
