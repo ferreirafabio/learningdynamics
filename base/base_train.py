611c2067fa1eccdb05f3218f01747a064285b5a9
ferreirafabio <fabioferreira@mailbox.org>
2019-04-15 01:28:04 -0700

    added multi step configs

--- base/base_train.py
+++ base/base_train.py
@@ -68,7 +68,7 @@ class BaseTrain:
         self.model.is_training = True
         self.model.output_ops_train = self.model(self.model.input_ph, self.model.input_ctrl_ph, self.model.target_ph,
                                                  self.config.n_rollouts, self.model.is_training,
-                                                 do_multi_step_prediction_at_test=self.config.do_multi_step_prediction)
+                                                 do_multi_step_prediction=self.config.do_multi_step_prediction)
 
         total_loss_ops, loss_ops_img, loss_ops_iou, loss_ops_velocity, loss_ops_position, loss_ops_distance, targ = create_loss_ops(self.config, self.model.target_ph, self.model.output_ops_train)
         ''' remove all inf values --> correspond to padded entries '''
@@ -101,7 +101,7 @@ class BaseTrain:
         self.model.output_ops_test = self.model(self.model.input_ph_test, self.model.input_ctrl_ph_test,
                                                 self.model.target_ph_test, self.config.n_rollouts,
                                                 self.model.is_training,
-                                                do_multi_step_prediction_at_test=self.config.do_multi_step_prediction)
+                                                do_multi_step_prediction=self.config.do_multi_step_prediction)
         total_loss_ops_test, loss_ops_test_img, loss_ops_test_iou, loss_ops_test_velocity, loss_ops_test_position, loss_ops_test_distance, targ = create_loss_ops(self.config, self.model.target_ph_test, self.model.output_ops_test)
 
         ''' remove all inf values --> correspond to padded entries '''
