f01e1a16890a23d5311e86159c70c6817e80a77a
fabio.ferreira@student.kit.edu <f4bio.ferreira@gmail.com>
2019-05-11 14:35:25 -0700

    storing latent vectors

--- base/base_train.py
+++ base/base_train.py
@@ -90,7 +90,7 @@ class BaseTrain:
         self.model.output_ops_train, self.model.latent_init_img_train = self.model(self.model.input_ph, self.model.target_ph,
                                                  1, self.model.is_training)
 
-        total_loss_ops, loss_ops_img, loss_ops_iou, loss_ops_velocity, loss_ops_position, loss_ops_distance, _ = create_loss_ops(self.config, self.model.target_ph, self.model.output_ops_train)
+        total_loss_ops, loss_ops_img, loss_ops_iou, loss_ops_velocity, loss_ops_position, loss_ops_distance, loss_ops_global = create_loss_ops(self.config, self.model.target_ph, self.model.output_ops_train)
         ''' remove all inf values --> correspond to padded entries '''
         self.model.loss_op_train_total = total_loss_ops
         self.model.loss_ops_train_img = loss_ops_img  # just for summary, is already included in loss_op_train
@@ -98,6 +98,7 @@ class BaseTrain:
         self.model.loss_ops_train_velocity = loss_ops_velocity
         self.model.loss_ops_train_position = loss_ops_position
         self.model.loss_ops_train_distance = loss_ops_distance
+        self.model.loss_ops_train_global = loss_ops_global
         #self.model.train_logits = logits
 
         self.model.step_op = self.model.optimizer.minimize(self.model.loss_op_train_total, global_step=self.model.global_step_tensor)
@@ -117,7 +118,7 @@ class BaseTrain:
                                                 self.model.target_ph_test, 1,
                                                 self.model.is_training)
 
-        total_loss_ops_test, loss_ops_test_img, loss_ops_test_iou, loss_ops_test_velocity, loss_ops_test_position, loss_ops_test_distance, _ = create_loss_ops(self.config, self.model.target_ph_test, self.model.output_ops_test)
+        total_loss_ops_test, loss_ops_test_img, loss_ops_test_iou, loss_ops_test_velocity, loss_ops_test_position, loss_ops_test_distance, loss_ops_test_global = create_loss_ops(self.config, self.model.target_ph_test, self.model.output_ops_test)
 
         ''' remove all inf values --> correspond to padded entries '''
         self.model.loss_op_test_total = total_loss_ops_test
@@ -126,5 +127,6 @@ class BaseTrain:
         self.model.loss_ops_test_velocity = loss_ops_test_velocity
         self.model.loss_ops_test_position = loss_ops_test_position
         self.model.loss_ops_test_distance = loss_ops_test_distance
+        self.model.loss_ops_test_global = loss_ops_test_global
         #self.model.test_logits = logits
 
